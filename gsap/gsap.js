1c20cd622d416b20fb50999c7ba5455925f38f66
sg-sebastianguerrero <sebasgue2207@gmail.com>
2021-08-10 17:59:53 -0500

    added gsap effects

---
+++ gsap/gsap.js
@@ -0,0 +1,30 @@
+TweenMax.to('.first', 1.5, {
+    delay: .2,
+    left: '-100%',
+    ease: Expo.easeInOut
+  })
+
+  TweenMax.to('.second', 1.5, {
+    delay: .4,
+    left: '-100%',
+    ease: Expo.easeInOut
+  })
+
+  TweenMax.to('.third', 1.5, {
+    delay: .6,
+    left: '-100%',
+    ease: Expo.easeInOut
+  })
+
+/*   TweenMax.to('.text', 2, {
+    delay: 5.2,
+    left: '-100%',
+    ease: Expo.easeInOut
+  }) */
+
+  TweenMax.to('.text', 1, {
+    delay: 2.2,
+    opacity: 100,
+    left: 0,
+    ease: Expo.easeInOut
+  })
