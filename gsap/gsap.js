bf6743fb82b25b0ddc497e3bc0ba471bc5f7a80a
sg-sebastianguerrero <sebasgue2207@gmail.com>
2021-11-01 17:22:29 -0500

    make up commit

--- gsap/gsap.js
+++ gsap/gsap.js
@@ -1,26 +1,21 @@
+
 TweenMax.to('.first', 1.5, {
     delay: .2,
     left: '-100%',
     ease: Expo.easeInOut
   })
 
-  TweenMax.to('.second', 1.5, {
+TweenMax.to('.second', 1.5, {
     delay: .4,
     left: '-100%',
     ease: Expo.easeInOut
   })
 
-  TweenMax.to('.third', 1.5, {
+TweenMax.to('.third', 1.5, {
     delay: .6,
     left: '-100%',
     ease: Expo.easeInOut
   })
-
-/*   TweenMax.to('.text', 2, {
-    delay: 5.2,
-    left: '-100%',
-    ease: Expo.easeInOut
-  }) */
 
   TweenMax.to('.text', 1, {
     delay: 2.2,
@@ -29,17 +24,19 @@ TweenMax.to('.first', 1.5, {
     ease: Expo.easeInOut
   })
 
-  
-/*   TweenMax.to('.logofolio', 1, {
-    delay: 5,
-    opacity: 100,
-    left: 0, 
-    ease: Expo.easeInOut
-  })
+gsap.registerPlugin(ScrollTrigger)
 
-  TweenMax.to('.projects', 1, {
-    delay: 6,
-    opacity: 100,
-    left: 0, 
-    ease: Expo.easeInOut
-  }) */
+gsap.to(".projects", {
+  scrollTrigger:{
+    trigger: '.projects',
+    //markers: true, 
+    start: 'top top',
+    //start: '=-300 0',
+    duration: 3,
+    scrub: 5,
+    //pin: '.projects',
+  },
+  opacity: 100,
+});
+  
+
